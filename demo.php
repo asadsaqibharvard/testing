63a045b93cabbdb8fe13bdee50f42b91c9c00780
Asad Saqib <asadsaqib@fas.harvard.edu>
2013-10-19 21:08:24 -0400

    dsfsd

---
+++ demo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php require_once('logic.php'); ?>
+</head>
+
+<body>
+	The square of 4 is <?=$square?>
+
+	test line
+
+	<p> testingsa;lkjfslfjs </p>
+</body>
+
+</html>
